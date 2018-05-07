412bbee23a1457bfbb8c5945325e55ada83bc89a
satyanarayanapalli <pallisatyanarayan@gmail.com>
2018-05-07 18:26:47 +0530

    fourth commit

---
+++ file append example/service.js
@@ -0,0 +1,52 @@
+var http=require("http");
+var port=9004;
+var fs=require("fs");
+
+var app=http.createServer(function(req,res)
+{
+    fs.readFile("sample.txt",function(err,data)
+    {
+        if(err)
+        {
+            console.log("Error occured..");
+        }
+        else{
+            res.writeHead(200,{"Content-type":"text/html"});
+            res.write(data);
+            res.end();
+            }
+        });
+  
+
+    fs.appendFile("sample.txt","       I am added now by using appendFile method",function(err)
+    {
+        if(err)throw error
+        {
+            console.log("while appending error occured..")
+        }
+    });
+
+
+
+    fs.readFile("sample.txt",function(err,data)
+    {
+        if(err)
+        {
+            console.log("Error occured..");
+        }
+        else{
+            res.writeHead(200,{"Content-type":"text/html"});
+            res.write("</br>");
+            res.write(data);
+            res.end();
+            }
+        });
+
+
+
+});
+
+app.listen(port,function()
+{
+    console.log("listening port "+port); 
+});
